e4c5345ba44987649d03a59732b024fb5e0639a7
Stelio <sedyhm2211@gmail.com>
2020-07-24 15:24:17 +0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+class App extends React.Component {
+    render() {
+        return <Container>
+            <h1>Hello Stormotion!</h1>
+        </Container>;
+    }
+}
+
+export default App;
